8d578e451f1f85df3aa73fb78d60925e67f1f97e
Taito <taito.tanaka1211@gmail.com>
2021-02-04 12:52:18 +0900

    change

--- myblog/settings.py
+++ myblog/settings.py
@@ -127,8 +127,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_URL = '/media/markdownx/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/markdownx')
 
 """ マークダウンのオプション設定 """
 MARKDOWNX_MARKDOWN_EXTENSIONS = [
